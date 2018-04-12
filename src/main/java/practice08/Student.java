3bac479e52299abb3e3c8541bee728fe53a529ef
刘云志 <liuyunzhi@chinamcloud.com>
2018-04-12 14:55:39 +0800

    完成practice08

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,23 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (this.equals(this.klass.getLeader())) {
+            return super.introduce() + " I am a Student. I am Leader of " + this.klass.getDisplayName() + ".";
+        } else {
+            return super.introduce() + " I am a Student. I am at " + this.klass.getDisplayName() + ".";
+        }
+    }
 }
