39db35e62d5184c2304afb1e44c4cf4b04466d57
刘云志 <liuyunzhi@chinamcloud.com>
2018-04-12 16:48:59 +0800

    完成practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,43 @@
 package practice10;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private Set<Student> students;
+
+    public Klass(int number) {
+        this.number = number;
+        this.students = new HashSet<>();
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student leader) {
+        if (students.contains(leader)) {
+            this.leader = leader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
+
+    public boolean isIn(Student student) {
+        return students.contains(student);
+    }
 }
--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,43 @@
 package practice10;
 
 public class Person {
+    protected int id;
+    protected String name;
+    protected int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name+". I am " + this.age+" years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o != null && o.getClass() == Person.class) {
+            Person person = (Person)o;
+            if (this.id == person.getId()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
