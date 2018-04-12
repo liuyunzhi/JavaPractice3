506166be6d3f4beff62fe150f615db9f9c71fae7
刘云志 <liuyunzhi@chinamcloud.com>
2018-04-12 12:54:18 +0800

    完成practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String namme, int age) {
+        super(namme, age);
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        String prefix = super.introduce() + " I am a Teacher. I teach ";
+        if (this.klass != 0){
+            return prefix + "Class " + this.klass + ".";
+        } else {
+            return prefix + "No Class.";
+        }
+    }
 }
