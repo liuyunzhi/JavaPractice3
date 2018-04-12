d55feaf45b2fe333695f6fe0edb20aa3c482f35b
刘云志 <liuyunzhi@chinamcloud.com>
2018-04-12 15:12:51 +0800

    完成practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> students;
+
+    public Klass(int number) {
+        this.number = number;
+        this.students = new ArrayList<>();
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student leader) {
+        if (students.contains(leader)) {
+            this.leader = leader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,39 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    private String myInformation() {
+        return super.introduce() + " I am a Teacher.";
+    }
+
+    @Override
+    public String introduce() {
+        if (this.klass == null) {
+            return this.myInformation() + " I teach No Class.";
+        } else {
+            return this.myInformation() + " I teach " + this.klass.getDisplayName() + ".";
+        }
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass() == this.klass) {
+            return this.myInformation() + " I teach " + student.getName() + ".";
+        } else {
+            return this.myInformation() + " I don't teach " + student.getName() + ".";
+        }
+    }
 }
